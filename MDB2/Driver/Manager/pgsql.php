2e6f58aab50383bc4d560bc5d1b3c91702ab0c02
Colin Viebrock <colin@viebrock.ca>
2025-07-29 11:21:58 -0400

    Re-add comments that were removed

--- MDB2/Driver/Manager/pgsql.php
+++ MDB2/Driver/Manager/pgsql.php
@@ -232,18 +232,72 @@ class MDB2_Driver_Manager_pgsql extends MDB2_Driver_Manager_Common
      * @param array  $changes associative array that contains the details of each type
      *                        of change that is intended to be performed. The types of
      *                        changes that are currently supported are defined as follows:
+     *                          name
+     *                            New name for the table.
+     *                          add
+     *                            Associative array with the names of fields to be added as
+     *                            indexes of the array. The value of each entry of the array
+     *                            should be set to another associative array with the properties
+     *                            of the fields to be added. The properties of the fields should
+     *                            be the same as defined by the MDB2 parser.
+     *                          remove
+     *                            Associative array with the names of fields to be removed as indexes
+     *                            of the array. Currently the values assigned to each entry are ignored.
+     *                            An empty array should be used for future compatibility.
+     *                          rename
+     *                            Associative array with the names of fields to be renamed as indexes
+     *                            of the array. The value of each entry of the array should be set to
+     *                            another associative array with the entry named name with the new
+     *                            field name and the entry named Declaration that is expected to contain
+     *                            the portion of the field declaration already in DBMS specific SQL code
+     *                            as it is used in the CREATE TABLE statement.
+     *                          change
+     *                            Associative array with the names of the fields to be changed as indexes
+     *                            of the array. Keep in mind that if it is intended to change either the
+     *                            name of a field and any other properties, the change array entries
+     *                            should have the new names of the fields as array indexes.
      *
-     *                             name
+     *                            The value of each entry of the array should be set to another associative
+     *                            array with the properties of the fields to that are meant to be changed as
+     *                            array entries. These entries should be assigned to the new values of the
+     *                            respective properties. The properties of the fields should be the same
+     *                            as defined by the MDB2 parser.
      *
-     *                                New name for the table.
+     *                          Example
      *
-     *                            add
+     *                              [
+     *                                  'name' => 'userlist',
+     *                                  'add'  => [
+     *                                      'quota' => [
+     *                                          'type'     => 'integer',
+     *                                          'unsigned' => 1,
+     *                                      ],
+     *                                   ],
+     *                                   'remove' => [
+     *                                      'file_limit' => [],
+     *                                      'time_limit' => [],
+     *                                   ],
+     *                                   'change' => [
+     *                                      'name' => [
+     *                                          'length'     => '20',
+     *                                          'definition' => [
+     *                                              'type'   => 'text',
+     *                                              'length' => 20,
+     *                                          ],
+     *                                      ],
+     *                                  ],
+     *                                  'rename' => [
+     *                                      'sex' => [
+     *                                          'name'       => 'gender',
+     *                                          'definition' => [
+     *                                              'type'    => 'text',
+     *                                              'length'  => 1,
+     *                                              'default' => 'M',
+     *                                          ],
+     *                                      ],
+     *                                  ],
+     *                              ]
      *
-     *                                Associative array with the names of fields to be added as
-     *                                 indexes of the array. The value of each entry of the array
-     *                                 should be set to another associative array with the properties
-     *                                 of the fields to be added. The properties of the fields should
-     *                                 be the same as defined by the MDB2 parser.
      * @param bool $check indicates whether the function should just check if the DBMS driver
      *                    can perform the requested table alterations if the value is true or
      *                    actually perform them otherwise
